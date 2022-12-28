80793ded108b61255b30d5ee668001167d81d549
Sergey Parshin <parshinsp@gmail.com>
2022-12-28 17:28:58 +0300

    2 project workflows update (#3)
    * Removed travis file
    
    * Added pre-commit config
    
    * Extended list of supported python versions
    
    * Added typing \ linter \ coverage configs in setup.cfg
    
    * Added python unittests workflow
    
    * Added linter workflow
    
    * Added mypy workflow
    
    * Added tox and changed python version in typing

--- configorm/config_orm.py
+++ configorm/config_orm.py
@@ -1,21 +1,27 @@
+import typing
+
 from .fields import Field
 
 SECTION_BASE = '_metaclass_helper_'
 
 
-def with_metaclass(meta, base=object):
+def with_metaclass(meta, base=object):  # type: ignore
+    """Utility function for metaclass connection"""
     return meta(SECTION_BASE, (base,), {})
 
 
 class Metadata(object):
-    def __init__(self, section, connector=None):
+    """Field binder metaclass"""
+
+    def __init__(self, section: typing.Any, connector: typing.Optional[typing.Any] = None) -> None:
         self.section = section
         self.connector = connector
 
-        self.fields = {}
+        self.fields: typing.Dict[str, Field] = {}
         self.name = section.__name__.lower()
 
-    def add_field(self, field_name, field):
+    def add_field(self, field_name: str, field: Field) -> None:
+        """Adds field into section and binds it"""
         if field_name not in self.fields:
             if isinstance(field, Field):
                 field.bind(self.section, field_name, self)
@@ -23,9 +29,11 @@ class Metadata(object):
 
 
 class SectionBase(type):
+    """Configuration Section setter"""
+
     inheritable = {'connector'}
 
-    def __new__(mcs, name, bases, attrs):
+    def __new__(mcs, name: str, bases: tuple, attrs: dict):  # type: ignore  # noqa: D102,N804
         if name == SECTION_BASE or bases[0].__name__ == SECTION_BASE:
             return super(SectionBase, mcs).__new__(mcs, name, bases, attrs)
 
@@ -49,7 +57,7 @@ class SectionBase(type):
 
         new_meta = meta_options.get('model_metadata_class', Metadata)
 
-        mcs = super(SectionBase, mcs).__new__(mcs, name, bases, attrs)
+        mcs = super(SectionBase, mcs).__new__(mcs, name, bases, attrs)  # type: ignore
         mcs.meta = new_meta(mcs, **meta_options)
 
         fields = []
@@ -62,7 +70,7 @@ class SectionBase(type):
 
         return mcs
 
-    def __setattr__(self, attr, val):
+    def __setattr__(self, attr: str, val: typing.Any) -> None:  # noqa: N804
         try:
             obj = object.__getattribute__(self, attr)
         except AttributeError:
@@ -78,12 +86,19 @@ class SectionBase(type):
                 object.__setattr__(self, attr, val)
 
 
-class Section(with_metaclass(SectionBase)):
-    def __init__(self, **kwargs):
+class Section(with_metaclass(SectionBase)):  # type: ignore
+    """Configuration Section class"""
+
+    def __init__(self, **kwargs) -> None:  # type: ignore
         pass
 
     @classmethod
-    def check_config_integrity(cls):
+    def check_config_integrity(cls) -> None:
+        """
+        Check if config with given sections and attributes exist. If not this method will create one and fill it with
+        default values.
+        :return: Nothing
+        """
         cls.meta.connector.create_config()
 
         for section in cls.__subclasses__():
@@ -92,4 +107,8 @@ class Section(with_metaclass(SectionBase)):
 
             for field_name, field_entry in section.meta.fields.items():
                 if cls.meta.connector.is_attr_exist(section_name=section.__name__, attr_name=field_name) is False:
-                    cls.meta.connector.add_attr(section_name=section.__name__, attr_name=field_name, value=field_entry.default)
+                    cls.meta.connector.add_attr(
+                        section_name=section.__name__,
+                        attr_name=field_name,
+                        value=field_entry.default
+                    )
